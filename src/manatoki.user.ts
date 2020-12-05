a33377d5786644fc3dc2e19ac2455aa2bed5da73
nanikit <nanikit4@gmail.com>
2020-12-06 07:31:19 +0900

    build: update dependency

--- src/manatoki.user.ts
+++ src/manatoki.user.ts
@@ -76,5 +76,5 @@ initialize(manatokiSource);
 // @resource     react-dom        https://cdn.jsdelivr.net/npm/react-dom@17.0.1/umd/react-dom.development.js
 // @resource     @stitches/core   https://cdn.jsdelivr.net/npm/@stitches/core@0.0.3-canary.4/dist/core.cjs.dev.js
 // @resource     @stitches/react  https://cdn.jsdelivr.net/npm/@stitches/react@0.0.3-canary.4/dist/react.cjs.dev.js
-// @resource     vim_comic_viewer https://greasyfork.org/scripts/417893-vim-comic-viewer/code/vim%20comic%20viewer.js?version=877077
+// @resource     vim_comic_viewer https://greasyfork.org/scripts/417893-vim-comic-viewer/code/vim%20comic%20viewer.js?version=877253
 // ==/UserScript==
