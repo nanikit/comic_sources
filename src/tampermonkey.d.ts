fd88a99824aff0c7088f5de0bd78a5fb6c40b661
nanikit <nanikit4@gmail.com>
2021-06-06 21:06:01 +0900

    refactor: adapt declaration file for deno

--- src/tampermonkey.d.ts
+++ src/tampermonkey.d.ts
@@ -1,28 +1,32 @@
-declare const GM_setValue: (key: string, value: any) => void;
-declare const GM_getValue: (key: string, fallback: any) => any;
-declare const GM_openInTab: (url: string) => void;
-type GmXhr =
-  & {
-    responseHeaders: Record<string, string>;
-  }
-  & Pick<
-    XMLHttpRequest,
-    | "readyState"
-    | "status"
-    | "statusText"
-    | "response"
-    | "responseXML"
-    | "responseText"
-  >;
-declare const GM_xmlhttpRequest: (request: {
-  method?: "GET" | "POST";
-  url: string;
-  headers?: object;
-  responseType?: XMLHttpRequest["responseType"];
-  data?: object;
-  onload?: (response: GmXhr) => void;
-  onerror?: (response: GmXhr) => void;
-  onabort?: (response: GmXhr) => void;
-}) => void;
-declare const unsafeWindow: Window & typeof globalThis;
-declare const exportFunction: <T extends Function>(fn: T, target: Window) => T;
+export {};
+
+declare global {
+  export const GM_setValue: (key: string, value: any) => void;
+  export const GM_getValue: (key: string, fallback: any) => any;
+  export const GM_openInTab: (url: string) => void;
+  export type GmXhr =
+    & {
+      responseHeaders: Record<string, string>;
+    }
+    & Pick<
+      XMLHttpRequest,
+      | "readyState"
+      | "status"
+      | "statusText"
+      | "response"
+      | "responseXML"
+      | "responseText"
+    >;
+  export const GM_xmlhttpRequest: (request: {
+    method?: "GET" | "POST";
+    url: string;
+    headers?: object;
+    responseType?: XMLHttpRequest["responseType"];
+    data?: object;
+    onload?: (response: GmXhr) => void;
+    onerror?: (response: GmXhr) => void;
+    onabort?: (response: GmXhr) => void;
+  }) => void;
+  export const unsafeWindow: Window & typeof globalThis;
+  export const exportFunction: <T extends Function>(fn: T, target: Window) => T;
+}
