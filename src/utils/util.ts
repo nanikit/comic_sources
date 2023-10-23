d0c1bdd791d6207bbd4a290d4f90b392ddbf5e98
nanikit <nanikit4@gmail.com>
2023-10-23 23:10:25 +0900

    chore: restore update tool

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,7 +1,2 @@
 export const timeout = (millisecond: number) =>
   new Promise((resolve) => setTimeout(resolve, millisecond));
-
-export const getText = async (url: string): Promise<string> => {
-  const response = await fetch(url);
-  return response.text();
-};
---
+++ tool/update_deps.ts
@@ -0,0 +1,48 @@
+import { expandGlob } from "https://deno.land/std@0.204.0/fs/expand_glob.ts";
+import { escape } from "https://deno.land/std@0.204.0/regexp/escape.ts";
+
+if (import.meta.main) {
+  await main();
+}
+
+async function main() {
+  const url = await getLatestUrl();
+  console.log(`Latest url: ${url}`);
+
+  const promises = [];
+  const files = expandGlob("./src/*.user.ts", {
+    exclude: ["./src/*.local.user.ts"],
+  });
+  for await (const entry of files) {
+    promises.push(rewrite(entry.path, url));
+  }
+  return Promise.all(promises);
+}
+
+async function rewrite(path: string, url: string): Promise<void> {
+  const source = await Deno.readTextFile(path);
+  const pattern = escape(
+    "https://greasyfork.org/scripts/417893-vim-comic-viewer/code/vim%20comic%20viewer.js?version=",
+  );
+  const regex = new RegExp(pattern + "\\d+", "g");
+  const rewritten = source.replace(regex, url);
+  if (rewritten === source) {
+    console.error(`Nothing changed: ${path}`);
+    return;
+  }
+
+  await Deno.writeTextFile(path, rewritten);
+}
+
+async function getLatestUrl() {
+  const html = await getText(
+    "https://greasyfork.org/ko/scripts/417893-vim-comic-viewer",
+  );
+  const url = html.match(/<code>\/\/ @require (.*?)<\/code>/);
+  return url?.[1]!;
+}
+
+async function getText(url: string): Promise<string> {
+  const response = await fetch(url);
+  return response.text();
+}
