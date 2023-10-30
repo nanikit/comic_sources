02ae5be3555ec331793439b0e15d4ca192fd0a3a
nanikit <nanikit4@gmail.com>
2023-10-31 03:44:54 +0900

    fix: not intended scroll when initializing viewer

--- src/arca.user.ts
+++ src/arca.user.ts
@@ -12,7 +12,7 @@
 // @connect        namu.la
 // @connect        *
 // @require        https://cdn.jsdelivr.net/npm/requirejs@2.3.6/require.js
-// @resource       link:vim_comic_viewer https://greasyfork.org/scripts/417893-vim-comic-viewer/code/vim%20comic%20viewer.js?version=1271986
+// @resource       link:vim_comic_viewer https://greasyfork.org/scripts/417893-vim-comic-viewer/code/vim%20comic%20viewer.js?version=1272833
 // ==/UserScript==
 
 import { main } from "./sites/arca.ts";
